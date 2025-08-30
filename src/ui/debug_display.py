f3ab08bbcf32bcb32b1098751133b544312b3391
kodakar <kodakarar@gmail.com>
2025-08-31 02:10:13 +0900

    ファイル削除

--- src/ui/debug_display.py
+++
@@ -1,4 +0,0 @@
-class DebugDisplay:
-    def render(self, frame, data):
-        # デバッグ用表示
-        pass
--- src/ui/test_display.py
+++
@@ -1,7 +0,0 @@
-class TestDisplay:
-    def render(self, frame, data):
-        # テスト用表示
-        passclass TestDisplay:
-    def render(self, frame, data):
-        # テスト用表示
-        pass
